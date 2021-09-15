6510e783c0fa192a02ef71e44eaaeed030a566fa
Girish Guptha Vayugundla <girish.v@tvsnext.io>
2021-09-15 11:44:38 +0530

    refactor the code using POM and page factory

--- Facebook_DataDrivenFramework/DoAction/Action.cs
+++ Facebook_DataDrivenFramework/DoAction/Action.cs
@@ -6,13 +6,25 @@ using System.Text;
 
 namespace Facebook_DataDrivenFramework.DoAction
 {
-    class Action
+    public class Action
     {
         public static void AssertAfterLauching(IWebDriver driver)
         {
             string title1 = "Facebook – log in or sign up";
             string title = driver.Title;
             Assert.AreEqual(title1, title);
+        }
+
+        public static void LoginToFaceBook(IWebDriver driver)
+        {
+            driver.FindElement(By.Name("email")).SendKeys(ExcelOperations.ReadData(1, "email"));
+            System.Threading.Thread.Sleep(3000);
+
+            driver.FindElement(By.Id("pass")).SendKeys(ExcelOperations.ReadData(1, "password"));
+            System.Threading.Thread.Sleep(3000);
+
+            driver.FindElement(By.Name("login")).Click();
+            System.Threading.Thread.Sleep(10000);
         }
     }
 }
---
+++ Facebook_DataDrivenFramework/UnitTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facebook_DataDrivenFramework
+{
+    class UnitTest:Base.BaseClass
+    {
+        [Test]
+        public void ReadingData()
+        {
+            ExcelOperations.PopulateInCollection(@"C:\Users\girish.v\source\repos\Facebook_DataDrivenFramework\Facebook_DataDrivenFramework\ExcelData\TestData.xlsx");
+        }
+    }
+}
