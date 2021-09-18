9bfa6c6178051217ede86bfce37c25d2042e6744
Girish Guptha Vayugundla <girish.v@tvsnext.io>
2021-09-18 13:50:36 +0530

    [girish]gentarting the report for the application

---
+++ Facebook_DataDrivenFramework/ReportClass.cs
@@ -0,0 +1,37 @@
+﻿using AventStack.ExtentReports;
+using AventStack.ExtentReports.Reporter;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Facebook_DataDrivenFramework
+{
+    class ReportClass
+    {
+        public static ExtentHtmlReporter htmlReporter;
+        public static ExtentReports extent;
+        public static ExtentTest test;
+
+        public static ExtentReports report()
+        {
+            if (extent == null)
+            {
+                string reportPath = @"C:\Users\girish.v\source\repos\Facebook_DataDrivenFramework\Facebook_DataDrivenFramework\report\Report.html";
+                htmlReporter = new ExtentHtmlReporter(reportPath);
+                extent = new ExtentReports();
+                extent.AttachReporter(htmlReporter);
+                extent.AddSystemInfo("OS", "Windows");
+                extent.AddSystemInfo("UserName", "girish");
+                extent.AddSystemInfo("ProviderName", "girish");
+                extent.AddSystemInfo("Domain", "QA");
+                extent.AddSystemInfo("ProjectName", "Ajio Automation");
+
+                string conifgPath = @"C:\Users\girish.v\source\repos\Facebook_DataDrivenFramework\Facebook_DataDrivenFramework\Report.xml";
+                htmlReporter.LoadConfig(conifgPath);
+
+
+            }
+            return extent;
+        }
+    }
+}
--- Facebook_DataDrivenFramework/UnitTest.cs
+++ Facebook_DataDrivenFramework/UnitTest.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using AventStack.ExtentReports;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -7,13 +8,21 @@ namespace Facebook_DataDrivenFramework
 {
     class UnitTest:Base.BaseClass
     {
+        ExtentReports reports = ReportClass.report();
+        ExtentTest test;
         [Test]
         public void ReadingData()
         {
+            test = reports.CreateTest("Tests");
+            test.Log(Status.Info, "Automating Spotify Login PAge");
+
             ExcelOperations.PopulateInCollection(@"C:\Users\girish.v\source\repos\Facebook_DataDrivenFramework\Facebook_DataDrivenFramework\ExcelData\TestData.xlsx");
             DoAction.Action.LoginToFaceBook(driver);
             DoAction.Action.SearchKey(driver);
             Takescreenshot();
+
+            test.Log(Status.Pass, "Test PAsses");
+            reports.Flush();
         }
       
     }
